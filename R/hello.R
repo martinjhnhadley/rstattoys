bdcb0df0487321a6a66319c9216d2a6935a7d159
Martin Hadley <martinjohnhadley@gmail.com>
2015-10-27 09:34:30 +0000

    Description

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,13 @@
 hello <- function() {
   print("Hello, world!")
 }
+if(TRUE){
+  x=1  # inline comments
+}else{
+  x=2;print('Oh no... ask the right bracket to go away!')}
+1*3 # one space before this comment will become two!
+2+2+2    # only 'single quotes' are allowed in comments
+
+lm(y~x1+x2, data=data.frame(y=rnorm(100),x1=rnorm(100),x2=rnorm(100)))  ### a linear model
+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1  ## comments after a long line
+## here is a long long long long long long long long long long long long long comment which will be wrapped
